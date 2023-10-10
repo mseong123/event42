246ed77e22a07bd4af9a1a94770b8f4c60898bbf
Meng Seong Lee <melee@u82z06s01.42kl.edu.my>
2023-10-10 14:21:43 +0800

    done

--- next-auth.js
+++ next-auth.js
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import FortyTwoProvider from "next-auth/providers/42-school";
 
 export default NextAuth({
   providers: [
-    Providers.FortyTwo({
+    FortyTwoProvider({
       clientId: process.env.FORTYTWO_CLIENT_ID,
       clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
     }),
